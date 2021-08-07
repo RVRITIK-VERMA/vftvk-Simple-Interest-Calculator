9366fbd9a5948eeb2cf7088e83a3df38c2d6471c
Ritik Verma <ritikverma431@gmail.com>
2021-08-07 12:01:57 +0530

    Final project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function validation(){
+function validation() {
     var principal = document.getElementById("principal").value;
-    if(principal<=0){
+    if (principal <= 0) {
         alert("Enter a positive number")
         document.getElementById("principal").focus();
         return false;
@@ -8,15 +8,15 @@ function validation(){
     return true;
 }
 function compute() {
-    if(validation()){
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);
-    var result=interest;
+    if (validation()) {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        var result = interest;
 
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+result+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\><br>"
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + result + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\><br>"
     }
 }
 function updateRate() {
